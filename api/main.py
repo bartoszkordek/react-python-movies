647d1fb4cc1b0592e47e824a6bbdb4dbeee9275e
Bartosz Kordek <bartosz.kordek10@gmail.com>
2026-01-29 20:53:16 +0100

    display 'actors' list

--- api/main.py
+++ api/main.py
@@ -11,28 +11,32 @@ class Movie(BaseModel):
     year: str
     actors: list[str]
 
+
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="../ui/build/static", check_dir=False), name="static")
 
+
 @app.get("/")
 def serve_react_app():
-   return FileResponse("../ui/build/index.html")
+    return FileResponse("../ui/build/index.html")
+
 
 @app.get('/movies')
-def get_movies():  # put application's code here
+def get_movies():
     db = sqlite3.connect('movies.db')
     cursor = db.cursor()
     movies = cursor.execute('SELECT * FROM movies')
 
     output = []
     for movie in movies:
-         movie = {'id': movie[0], 'title': movie[1], 'year': movie[2], 'actors': movie[3]}
-         output.append(movie)
+        movie = {'id': movie[0], 'title': movie[1], 'year': movie[2], 'actors': movie[3]}
+        output.append(movie)
     return output
 
+
 @app.get('/movies/{movie_id}')
-def get_single_movie(movie_id:int):  # put application's code here
+def get_single_movie(movie_id: int):  # put application's code here
     db = sqlite3.connect('movies.db')
     cursor = db.cursor()
     movie = cursor.execute(f"SELECT * FROM movies WHERE id={movie_id}").fetchone()
@@ -40,35 +44,37 @@ def get_single_movie(movie_id:int):  # put application's code here
         return {'message': "Movie not found"}
     return {'title': movie[1], 'year': movie[2], 'actors': movie[3]}
 
+
 @app.post("/movies")
 def add_movie(movie: Movie):
     flat_actors = ", ".join(movie.actors)
     db = sqlite3.connect('movies.db')
     cursor = db.cursor()
-    cursor.execute(f"INSERT INTO movies (title, year, actors) VALUES ('{movie.title}', '{movie.year}', '{flat_actors}')")
+    cursor.execute(
+        f"INSERT INTO movies (title, year, actors) VALUES ('{movie.title}', '{movie.year}', '{flat_actors}')")
     db.commit()
     return {
         "message": f"Movie with id = {cursor.lastrowid} added successfully",
         "id": cursor.lastrowid
     }
-    # movie = models.Movie.create(**movie.dict())
-    # return movie
+
 
 @app.put("/movies/{movie_id}")
-def update_movie(movie_id:int, params: dict[str, Any]):
+def update_movie(movie_id: int, params: dict[str, Any]):
     db = sqlite3.connect('movies.db')
     cursor = db.cursor()
     cursor.execute(
-    "UPDATE movies SET title = ?, year = ?, actors = ? WHERE id = ?",
-    (params['title'], params['year'], params['actors'], movie_id)
+        "UPDATE movies SET title = ?, year = ?, actors = ? WHERE id = ?",
+        (params['title'], params['year'], params['actors'], movie_id)
     )
     db.commit()
     if cursor.rowcount == 0:
         return {"message": f"Movie with id = {movie_id} not found"}
     return {"message": f"Movie with id = {cursor.lastrowid} updated successfully"}
 
+
 @app.delete("/movies/{movie_id}")
-def delete_movie(movie_id:int):
+def delete_movie(movie_id: int):
     db = sqlite3.connect('movies.db')
     cursor = db.cursor()
     cursor.execute("DELETE FROM movies WHERE id = ?", (movie_id,))
@@ -77,8 +83,9 @@ def delete_movie(movie_id:int):
         return {"message": f"Movie with id = {movie_id} not found"}
     return {"message": f"Movie with id = {movie_id} deleted successfully"}
 
+
 @app.delete("/movies")
-def delete_movies(movie_id:int):
+def delete_movies():
     db = sqlite3.connect('movies.db')
     cursor = db.cursor()
     cursor.execute("DELETE FROM movies")
@@ -86,5 +93,11 @@ def delete_movies(movie_id:int):
     return {"message": f"Deleted {cursor.rowcount} movies"}
 
 
-# if __name__ == '__main__':
-#     app.run()
+@app.get('/actors')
+def get_movies():
+    db = sqlite3.connect('movies.db')
+    cursor = db.cursor()
+    movies = cursor.execute('SELECT actors FROM movies')
+    result = sorted({name for row in movies for name in row[0].split(', ') if name})
+    response_body = [{'name': actor} for actor in result]
+    return response_body
--- ui/src/App.js
+++ ui/src/App.js
@@ -3,10 +3,12 @@ import {useEffect, useState} from "react";
 import "milligram";
 import MovieForm from "./MovieForm";
 import MoviesList from "./MoviesList";
+import ActorsList from "./ActorsList";
 
 function App() {
     const [movies, setMovies] = useState([]);
     const [addingMovie, setAddingMovie] = useState(false);
+    const [actors, setActors] = useState([]);
 
     const fetchMovies = async () => {
             const response = await fetch(`/movies`);
@@ -15,8 +17,16 @@ function App() {
                 setMovies(movies);
             }
          };
+    const fetchActors = async () => {
+            const response = await fetch(`/actors`);
+            if (response.ok) {
+                const actors = await response.json();
+                setActors(actors);
+            }
+         };
     useEffect(() => {
         fetchMovies();
+        fetchActors();
     }, []);
 
     async function handleAddMovie(movie) {
@@ -28,7 +38,8 @@ function App() {
           if (response.ok) {
               setMovies([...movies, movie]);
               setAddingMovie(false);
-              fetchMovies()
+              fetchMovies();
+              fetchActors();
           }
     }
 
@@ -40,23 +51,35 @@ function App() {
             });
             if (response.ok) {
                 setMovies(movies.filter(m => m.id !== movie.id))
-                fetchMovies()
+                fetchMovies();
+                fetchActors();
             }
     }
 
     return (
         <div className="container">
-            <h1>My favourite movies to watch</h1>
-            {movies.length === 0
-                ? <p>No movies yet. Maybe add something?</p>
-                : <MoviesList movies={movies}
-                              onDeleteMovie={handleDeleteMovie}
-                />}
-            {addingMovie
-                ? <MovieForm onMovieSubmit={handleAddMovie}
-                             buttonLabel="Add a movie"
-                />
-                : <button onClick={() => setAddingMovie(true)}>Add a movie</button>}
+             <h1>My favourite movies to watch</h1>
+            <div className="row">
+                <div className="column column-60">
+                    {movies.length === 0
+                        ? <p>No movies yet. Maybe add something?</p>
+                        : <MoviesList movies={movies}
+                                      onDeleteMovie={handleDeleteMovie}
+                        />}
+                    {addingMovie
+                        ? <MovieForm onMovieSubmit={handleAddMovie}
+                                     buttonLabel="Add a movie"
+                        />
+                        : <button onClick={() => setAddingMovie(true)}>Add a movie</button>}
+                </div>
+                <div className="column column-40">
+                    {actors.length === 0
+                        ? <p>No actors yet.</p>
+                        : <ActorsList actors={actors}
+                        />}
+                </div>
+            </div>
+
         </div>
     );
 }
